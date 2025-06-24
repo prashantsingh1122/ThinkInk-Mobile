003f5ccd27f85725d794fb111f297695fb1133d6
prashantsingh1122 <prashantsingh3517@gmail.com>
2025-06-25 01:54:01 +0530

    initialcommit_1.2

---
+++ thinkink-mobile/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text
+      style={{fontSize:40}}>Welcome To ThinkInk Mobile</Text>
+    </View>
+  );
+}
